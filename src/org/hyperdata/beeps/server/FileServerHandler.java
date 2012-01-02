0400fd3a567686a583199226092ac2aecc74babf
Danny Ayers <danny.ayers@gmail.com>
2012-01-02 12:49:04 +0100

    missing handlers

---
+++ src/org/hyperdata/beeps/server/FileServerHandler.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.hyperdata.beeps.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
+/**
+ * @author danny
+ *
+ */
+public class FileServerHandler extends ResourceHandler{
+	
+	public FileServerHandler(){
+		setDirectoriesListed(true);
+		setWelcomeFiles(new String[] { "index.html" });
+		setResourceBase("www");
+	}
+	
+	public void handle(String target, Request baseRequest,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		System.out.println(target);
+//		if(target.startsWith("/audio/")){
+//			target = "/audio/" + escapeSlashes(target.substring(7, target.length()));
+//			System.out.println("new="+target);
+//		}
+		
+		super.handle(target, baseRequest, request, response);
+	}
+
+	/**
+	 * @param substring
+	 * @return
+	 */
+	public static String escapeSlashes(String string) {
+		string = string.replaceAll("/", "SLASH");
+		return string;
+	}
+
+}
